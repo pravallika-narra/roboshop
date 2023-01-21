6ca128172c2e75181b2d8daeaf31f80171f7f86d
pravallika <pravallikanarra99@gmail.com>
2023-01-21 06:24:01 +0530

    reddis

--- redis.sh
+++ redis.sh
@@ -7,7 +7,7 @@ dnf module enable redis:remi-6.2 -y
 
 yum install redis -y
 
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf/
 
 systemctl enable redis
 systemctl start redis
