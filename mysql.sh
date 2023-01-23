45383efdb5762e855bc1ab7c320450e5d1e81c63
pravallika <pravallikanarra99@gmail.com>
2023-01-23 10:47:09 +0530

    mysql

---
+++ mysql.sh
@@ -0,0 +1,7 @@
+sp=$(pwd)
+dnf module disable mysql -y
+cp ${sp}/files/mysql.repo  /etc/yum.repos.d/mysql.repo
+yum install mysql-community-server -y
+systemctl enable mysqld
+systemctl start mysqld
+mysql_secure_installation --set-root-pass RoboShop@1
